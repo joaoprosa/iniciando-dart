f94de9ac619fc0c769900a1313a24c090164b7b5
Joao Paulo Rosa <joao.rosa@medbarra.com.br>
2019-12-16 20:23:48 -0300

    treino de funções

--- funcoes.dart
+++ funcoes.dart
@@ -1,20 +1,28 @@
 void main (){
   // boasVindas();
-  double resultadoSimulador = simulador(0.50, 0.50);
+  double resultadoSimulador = simulador(1, 1);
   print(resultadoSimulador);
 
+////////////////////////////////////////////////////
   print(calcArea(10));
 
 
   //Quando for chamar a função tem de por ela dentro de uma variável//
 }
-void boasVindas(){
-  String nome = 'Lucas';
-  print ('Bem vindo ' + nome);
-} 
+// void boasVindas(){
+//   String nome = 'Lucas';
+//   print ('Bem vindo ' + nome);
+// }
+
+//Aqui é um cauculo//
 double simulador(double valor1, double valor2){
+
   double res = valor1 + valor2;
   return res;
+  
 }
+//----------------------------//
 
+//Aqui é um cauculo//
 double calcArea (double raio) => 3.14 * raio * raio;
+//----------------------------//
---
+++ treino-dart/treino-funcoes.dart
@@ -0,0 +1,18 @@
+void main (){
+  
+  var resultadoConta = conta(50, 50);
+  print(resultadoConta);
+
+  //nome da função + abertura dos parametros + o valor dos parametros//
+  print(expressao(10, 10));
+
+}
+
+double conta(double numberOne, double numberTwo){
+    double result = numberOne + numberTwo;
+    return result;
+}
+
+//Quando se usa Arrow, na hora de chamar a função no main coloca-se o print e no lugar de object
+//coloca o nome da expressão, abre parenteses dnv como se fosse uma função e joga os parametros//
+double expressao(double numberThree, double numberFour) => 10 * numberThree + numberFour;
