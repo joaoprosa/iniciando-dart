75e40174610472f569545e773b895d7f5b07b370
Joao Paulo Rosa <joao.rosa@medbarra.com.br>
2020-01-31 16:12:57 -0300

    treino dezembro

--- treino-dezembro/treino-funcoes.dart
+++ treino-dezembro/treino-funcoes.dart
@@ -15,4 +15,4 @@ double conta(double numberOne, double numberTwo){
 
 //Quando se usa Arrow, na hora de chamar a função no main coloca-se o print e no lugar de object
 //coloca o nome da expressão, abre parenteses dnv como se fosse uma função e joga os parametros//
-double expressao(double numberThree, double numberFour) => 10 * numberThree + numberFour;
+double expressao(double numberThree, double numberFour) => 10 * numberThree + numberFour; 
