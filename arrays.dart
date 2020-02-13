af66198cdf94adeeb0bc1c0b61072d75d9c5ca8b
Joao Paulo Rosa <joao.rosa@medbarra.com.br>
2020-02-12 21:20:38 -0300

    Estudando array e loop

--- arrays.dart
+++ arrays.dart
@@ -1,24 +1,41 @@
 import 'dart:io';
 
-void main (){
+void main(){
 
-  var nome = [];
+  var nomes = ["Joao", "Paulo", "Gabriel"];
+  nomes.add("Andison");
+  // nomes.add(""); Eu poderia ficar adicionando quantos nomes quiser...
 
-  bool condicao = true;
-
-  while (condicao) {
-    print("Digite o nome");
-    String text = stdin.readLineSync();
-    if(text == "sair"){
-      print("=== Proigrama Finalizado");
-      condicao = false;
-    } else {
-      nome.add(text);
-    }
-    print(nome);
-    print("\n");
-  }
-
-
+  print(nomes.length);
+  print(nomes);
 
 }
+
+
+
+
+
+
+
+// void main (){
+
+//   var nome = [];
+
+//   bool condicao = true;
+
+//   while (condicao) {
+//     print("Digite o nome");
+//     String text = stdin.readLineSync();
+//     if(text == "sair"){
+//       print("=== Proigrama Finalizado");
+//       condicao = false;
+//     } else {
+//       nome.add(text);
+//     }
+//     print(nome);
+//     print("\n");
+//   }
+
+
+
+// }
